20b0844451e51f46f5c7219c71f51fadcf32771b
[TAREQUE-AHMED] <[tarequeahmed46@gmail.com]>
2024-09-19 07:54:45 +0600

    while-loop

---
+++ for-of.js
@@ -0,0 +1,16 @@
+const numbers = [10, 88, 99, 87, 65, 53, 18];
+console.log(numbers);
+
+for (const num of numbers) {
+    console.log(num);
+    
+}
+
+
+const fruits = ['banana', 'apple', 'mango', 'watermelon'];
+for (const fruit of fruits){
+    console.log(fruit);
+    console.log("I want to eat");
+    
+    
+}
